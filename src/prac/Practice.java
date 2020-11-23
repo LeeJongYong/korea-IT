1a0ad618cf1c15f53ee6c739c2b909f163722af6
LeeJongYong <jongyong924@gmail.com>
2020-11-24 00:22:40 +0900

    commit & push 연습

--- src/prac/Practice.java
+++ src/prac/Practice.java
@@ -3,5 +3,7 @@ package prac;
 public class Practice {
 	public static void main(String[] args) {
 		System.out.println("Git-Hub 연습");
+		System.out.println("commit & push 연습");
+		String temp = "practice";
 	}
 }
